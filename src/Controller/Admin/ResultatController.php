b8a868d072a4379f0faaf81ad4562debc322306e
bricelab <bricehessou@gmail.com>
2021-04-08 11:55:35 +0100

    feat: Ajout de l'export vers CSV

--- src/Controller/Admin/ResultatController.php
+++ src/Controller/Admin/ResultatController.php
@@ -5,10 +5,15 @@ namespace App\Controller\Admin;
 use App\Entity\Resultat;
 use App\Form\ResultatType;
 use App\Repository\ResultatRepository;
+use League\Csv\CannotInsertRecord;
+use League\Csv\Writer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use SplTempFileObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -54,7 +59,7 @@ class ResultatController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'resultat_show', methods: ['GET'])]
+    #[Route('/{id}/details', name: 'resultat_show', methods: ['GET'])]
     public function show(Resultat $resultat): Response
     {
         return $this->render('resultat/show.html.twig', [
@@ -103,5 +108,79 @@ class ResultatController extends AbstractController
         }
 
         return $this->redirectToRoute('resultat_index');
+    }
+
+    /**
+     * @param ResultatRepository $repository
+     * @return Response
+     * @IsGranted("ROLE_USER")
+     * @throws CannotInsertRecord
+     */
+    #[Route('/export-to-csv', name: 'resultat_export_csv', methods: ['GET'])]
+    public function exportToCsv(ResultatRepository $repository): Response
+    {
+        $resultats = $repository->findAll();
+//        dd($resultats);
+
+        $csvWriter = Writer::createFromFileObject(new SplTempFileObject());
+
+        $header = [
+            'Date',
+            'Département',
+            'Commune',
+            'Arrondissement',
+            'Inscrits',
+            'Votants',
+            'RLC',
+            'FCBE',
+            'Duo Talon.Talata',
+            'Votes nuls',
+            'Observations',
+        ];
+
+        $csvWriter->insertOne($header);
+
+        foreach ($resultats as $resultat) {
+            $data = [
+                $resultat->getCreatedAt()->format('d/m/Y H:i:s'),
+                $resultat->getArrondissement()->getCommune()->getDepartement()->getNom(),
+                $resultat->getArrondissement()->getCommune()->getNom(),
+                $resultat->getArrondissement()->getNom(),
+                $resultat->getArrondissement()->getNbInscrits(),
+                $resultat->getNbVotants(),
+                $resultat->getNbVoixRlc(),
+                $resultat->getNbVoixFcbe(),
+                $resultat->getNbVoixDuoTT(),
+                $resultat->getNbNuls(),
+                $resultat->getObservations(),
+            ];
+
+            $csvWriter->insertOne($data);
+        }
+
+        $flushThreshold = 1000; //the flush value should depend on your CSV size.
+        $contentCallback = function () use ($csvWriter, $flushThreshold) {
+            foreach ($csvWriter->chunk(1024) as $offset => $chunk) {
+                echo $chunk;
+                if ($offset % $flushThreshold === 0) {
+                    flush();
+                }
+            }
+        };
+
+        $response = new StreamedResponse();
+        $response->headers->set('Content-Encoding', 'none');
+        $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
+
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'remontees-presidentielles-2021.csv'
+        );
+
+        $response->headers->set('Content-Disposition', $disposition);
+        $response->headers->set('Content-Description', 'File Transfer');
+        $response->setCallback($contentCallback);
+
+        return $response;
     }
 }
